2ff13c2535edb1088684d33b1623fb5609646ad7
Sergio <sergio15032006@gmail.com>
2023-07-21 16:30:05 -0300

    Manage Credit Card tested and working

--- lib/app/modules/finance_manager/src/data/usecases/manage_credit_card.dart
+++ lib/app/modules/finance_manager/src/data/usecases/manage_credit_card.dart
@@ -1,4 +1,5 @@
 import 'package:result_dart/result_dart.dart';
+import 'package:umbrella_echonomics/app/modules/finance_manager/src/data/repositories/iinvoice_repository.dart';
 
 import '../../domain/entities/credit_card.dart';
 import '../../domain/usecases/imanage_credit_card.dart';
@@ -6,31 +7,33 @@ import '../../errors/errors.dart';
 import '../repositories/icredit_card_repository.dart';
 
 class ManageCreditCard implements IManageCreditCard {
-  final ICreditCardRepository repository;
+  final ICreditCardRepository cardRepository;
+  final IInvoiceRepository invoiceRepository;
 
-  ManageCreditCard(this.repository);
+  ManageCreditCard({
+    required this.cardRepository,
+    required this.invoiceRepository,
+  });
 
   @override
-  Future<Result<void, Fail>> cancel(CreditCard card) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<Result<void, Fail>> register(CreditCard card) async {
+    final cardCreateResult = await cardRepository.create(card);
+
+    if (cardCreateResult.isError()) {
+      return cardCreateResult;
+    }
+
+    return await invoiceRepository.generateOfCard(card);
   }
 
   @override
-  Future<Result<List<CreditCard>, Fail>> getAll() {
-    // TODO: implement getAll
-    throw UnimplementedError();
-  }
+  Future<Result<void, Fail>> update(CreditCard newCard) =>
+      cardRepository.updateCard(newCard);
 
   @override
-  Future<Result<void, Fail>> register(CreditCard card) {
-    // TODO: implement register
-    throw UnimplementedError();
-  }
+  Future<Result<List<CreditCard>, Fail>> getAll() => cardRepository.getAll();
 
   @override
-  Future<Result<void, Fail>> update(CreditCard newCard) {
-    // TODO: implement update
-    throw UnimplementedError();
-  }
+  Future<Result<void, Fail>> cancel(CreditCard card) =>
+      cardRepository.delete(card);
 }
