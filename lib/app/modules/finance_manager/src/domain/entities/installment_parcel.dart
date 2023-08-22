e0eb3c7937717a56d8a0a5d549aaa32e5c1ed985
Sergio <sergio15032006@gmail.com>
2023-08-21 21:15:15 -0300

    Mistake in my logic

--- lib/app/modules/finance_manager/src/domain/entities/installment_parcel.dart
+++ lib/app/modules/finance_manager/src/domain/entities/installment_parcel.dart
@@ -1,27 +1,25 @@
 import 'package:equatable/equatable.dart';
 
 import 'expense_parcel.dart';
-import 'invoice.dart';
-import 'paiyable.dart';
 
 class InstallmentParcel extends Equatable {
-  final Paiyable paiyable;
+  final ExpenseParcel parcel;
   final int parcelNumber;
 
   const InstallmentParcel({
-    required this.paiyable,
+    required this.parcel,
     required this.parcelNumber,
-  }) : assert(paiyable is Invoice || paiyable is ExpenseParcel);
+  });
 
   @override
-  List<Object?> get props => [paiyable, parcelNumber];
+  List<Object?> get props => [parcel, parcelNumber];
 
   InstallmentParcel copyWith({
-    Paiyable? paiyable,
+    ExpenseParcel? parcel,
     int? parcelNumber,
   }) {
     return InstallmentParcel(
-      paiyable: paiyable ?? this.paiyable,
+      parcel: parcel ?? this.parcel,
       parcelNumber: parcelNumber ?? this.parcelNumber,
     );
   }
