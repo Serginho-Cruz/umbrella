5550898ed7b54cc01b00ffd6dfdb97975b3e9b43
Sergio <sergio15032006@gmail.com>
2024-06-09 20:33:20 -0300

    I forgot about this

--- lib/app/modules/finance_manager/datasources.dart
+++ lib/app/modules/finance_manager/datasources.dart
@@ -1,6 +1,7 @@
 export 'src/external/datasources/account_datasource.dart';
 export 'src/external/datasources/balance_datasource.dart';
 export 'src/external/datasources/credit_card_datasource.dart';
+export 'src/external/datasources/invoice_datasource.dart';
 export 'src/external/datasources/expense_datasource.dart';
 export 'src/external/datasources/income_datasource.dart';
 export 'src/external/datasources/transaction_datasource.dart';
@@ -11,6 +12,7 @@ export 'src/infra/datasources/transaction_datasource.dart';
 export 'src/external/datasources/expense_type_datasource.dart';
 export 'src/external/datasources/income_type_datasource.dart';
 export 'src/infra/datasources/credit_card_datasource.dart';
+export 'src/infra/datasources/invoice_datasource.dart';
 export 'src/infra/datasources/expense_datasource.dart';
 export 'src/infra/datasources/income_datasource.dart';
 export 'src/infra/datasources/income_type_datasource.dart';
